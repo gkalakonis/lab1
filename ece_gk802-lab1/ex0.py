76fde4c9f3d81a1be84dd8bd1fc79d8bd7ada091
Georgios Kalakonis <up1083846@ac.upatras.gr>
2024-02-28 00:21:10 +0200

    Update ex0.py

--- ece_gk802-lab1/ex0.py
+++ ece_gk802-lab1/ex0.py
@@ -1,20 +1,28 @@
-import requests  # εισαγωγή της βιβλιοθήκης
+import requests  
+from datetime import datetime
 
 
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
 
+url = input("Enter url: ")
+if not url.startswith("http://"):
+    url="http://"+url
 
-url = 'http://python.org/'  # προσδιορισμός του url
+with requests.get(url) as response:  
+    website_headers=response.headers
+    print(f"Website: {url}\nHeaders{website_headers}")
+    server_info=website_headers.get("Server")
+    cookies_info=response.cookies
+   
+    if server_info:
+        print(f"\nServer information{server_info}")
+    else:
+        print("\nNo server information found")
 
-with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
-#
+    if cookies_info:
+        for cookie in cookies_info:
+            print(f"Cookie {cookie.name}={cookie.value}")
+            expires_date = datetime.utcfromtimestamp(round(cookie.expires)).strftime('%a, %d %b %Y %H:%M:%S GMT')#μετατροπή στην επιθυμητή μορφή
+            print(f"expires: {expires_date}")
+    else:
+        print("No cookies found")
+   
